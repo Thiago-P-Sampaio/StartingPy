16ab49665f6c975010a7b162b37723bfbad5dfe2
Thiago P <thiago.p.sampaio07@gmail.com>
2026-03-15 20:57:59 -0300

    :feat: tratando exceções

--- addnumber.py
+++ addnumber.py
@@ -1,14 +1,16 @@
 soma = 0
 quant = 0
 while True:
-    num = int(input("Digite um número(0 para sair): "))
-    if num == 0:
-        media = soma / quant
-        print("A média é: ", media, "\n", "Quantidade: ", quant)
-        break
-    else: 
-        soma += num
-        quant += 1
-    print("Soma: ", soma)
+    try:
+        num = int(input("Digite um número(0 para sair): "))
+        if num == 0:
+            media = soma / quant
+            print("A média é: ", media, "\n", "Quantidade: ", quant)
+            break
+        else: 
+            soma += num
+            quant += 1
+        print("Soma: ", soma)
 
-
+    except ValueError: 
+        print("Informe um valor válido")
